1f4154516dbde646ae9c5ffc1f6eed003006e478
alv <alv@DESKTOP-1R9N01Q>
2018-08-27 12:35:23 +0200

    26ago
    Change-Id: I655d1c47ef4bfee8d27701c1f174478fb26da0f4

--- Compartido/src/prueba/Pru.java
+++ Compartido/src/prueba/Pru.java
@@ -5,6 +5,8 @@ public class Pru {
 	public static void main(String[] args) {
 		System.out.println("Hola a todos");
 		System.out.println("De nuevo");
+		System.out.println("Otra vez más");
+		System.out.println("Mas");
 	}
 
 }
